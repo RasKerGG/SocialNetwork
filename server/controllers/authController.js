da5cfef3a75a6b998dd3a7b700bf040a8fdca005
Fedor24 <kurakin.fed@mail.ru>
2024-12-26 21:48:24 +0300

    Инициализия проекта, а также создание авторизации и регистрации(backend-часть)

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,5 @@
 import User from "../models/user.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcrypt"; //модель и bcrypt
 
 // Регистрация пользователя
 export const register = async (req, res) => {
